0187805f596d00d3bcdf8e651dc9fd83c06c1369
Aleshin <aleshincc@yandex.ru>
2020-03-29 16:48:58 +0300

    Project 4.4: Test coverage;

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,72 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from posts.models import Post
+
+User = get_user_model()
+
+# Create your tests here.
+
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+                        username="sarah", email="connor.s@skynet.com", password="12345678")
+        self.test_post = "Test post from sarah"
+
+    def test_Page_Profile(self):
+        response = self.client.get("/sarah/")
+        self.assertEqual(response.status_code, 200)
+
+    """
+    URL'ы в тестах можно генерировать по namespac'ам. Например:
+    url = reverse('archive', args=[1988])
+    assertEqual(url, '/archive/1988/')
+    Тем самым, если изменится url, можно будет править тесты 
+
+    Подскажите, пожалуйста, как этим воспользоваться в test_Page_Profile?
+    """
+    
+    def test_Authorization_User_Posting(self):
+        self.client.login(username='sarah', password='12345678')
+        self.client.post("/new/", {"text": self.test_post})
+        response = self.client.get("/")
+        self.assertContains(response, self.test_post)
+
+    def test_Unauthorized_User_Cannot_Post(self):
+        #self.client.logout() # Не нужно разлогиневаться так как при переходе на новый тест я автоматом разлогинелся
+        response = self.client.get('/new/')
+        self.assertRedirects(response, '/')
+
+    def test_New_Post_Appear_On_Desired_Pages(self):
+
+        new = Post.objects.create(author=self.user, text= self.test_post)
+        
+        response = self.client.get("/")
+        self.assertContains(response, self.test_post)
+
+        response = self.client.get("/sarah/")
+        self.assertContains(response, self.test_post)
+
+        response = self.client.get(f"/sarah/{new.id}/")
+        self.assertContains(response, self.test_post)
+
+    def test_Editing_End_Checking_Linked_Pages(self):
+
+        self.client.login(username='sarah', password='12345678') # Логинюсь
+        new = Post.objects.create(author=self.user, text= self.test_post) # Создаю пост
+        self.client.post(f"/sarah/{new.id}/edit", {"text": "modified"}) # Редактирую пост
+        self.assertEqual(Post.objects.get(author=self.user).text, "modified") # Изменился ли пост
+
+       
+        
+
+
+    
+
+
+
+
+
 # from django.test import TestCase
 # from django.test import Client
 
--- posts/views.py
+++ posts/views.py
@@ -5,12 +5,13 @@ from django.core.paginator import Paginator
 from .models import Post, Group, User
 from .forms import PostForm
 
+# Убрал лишнии комментарии, оставил только важные
+
 
 def index(request):
         
         post_list = Post.objects.order_by("-pub_date").all()
         paginator = Paginator(post_list, 10) # показывать по 10 записей на странице.
-
         page_number = request.GET.get('page') # переменная в URL с номером запрошенной страницы
         page = paginator.get_page(page_number) # получить записи с нужным смещением
 
@@ -18,21 +19,17 @@ def index(request):
 
 def group_posts(request, slug):
 
-
         # функция get_object_or_404 позволяет получить объект из базы данных 
         # по заданным критериям или вернуть сообщение об ошибке если объект не найден
         group = get_object_or_404(Group, slug=slug)
-
-        # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления
-        # условия WHERE group_id = {group_id}
-        posts = Post.objects.filter(group=group).order_by("-pub_date").all()
-        paginator = Paginator(posts, 10) # показывать по 10 записей на странице.
-
-        page_number = request.GET.get('page') # переменная в URL с номером запрошенной страницы
-        page = paginator.get_page(page_number) # получить записи с нужным смещением
+        posts = Post.objects.filter(group=group).order_by("-pub_date").all() #filter - аналог WHERE group_id = {group_id}
+        paginator = Paginator(posts, 10) 
+        page_number = request.GET.get('page') 
+        page = paginator.get_page(page_number)
         return render(request, "group.html", {"group": group, "posts": posts, 'paginator': paginator, 'page': page})
 
 def new_post(request):
+
         if request.user.is_authenticated : #Праверка авторизации
                 if request.method == 'POST':
                         form = PostForm(request.POST)
@@ -44,17 +41,16 @@ def new_post(request):
         return redirect('/') # Если пользователь не авторизован и пытается войти на стр new то его сразу перенаправляет на главную
 
 def profile(request, username):
-        # тут тело функции
+
         author_profile = User.objects.get(username=username)
         posts = Post.objects.filter(author=author_profile).order_by("-pub_date")
-
-        paginator = Paginator(posts, 10) # показывать по 10 записей на странице.
-        page_number = request.GET.get('page') # переменная в URL с номером запрошенной страницы
-        page = paginator.get_page(page_number) # получить записи с нужным смещением
+        paginator = Paginator(posts, 10) 
+        page_number = request.GET.get('page') 
+        page = paginator.get_page(page_number) 
         return render(request, "profile.html", {"posts": posts , "username": username, "author_profile": author_profile, 'page': page, 'paginator': paginator})
 
 def post_view(request, username, post_id):
-        # тут тело функции
+
         author_profile = User.objects.get(username=username)
         post = Post.objects.filter(author=author_profile).get(id=post_id)
         number = Post.objects.filter(author=author_profile).count()
@@ -62,12 +58,9 @@ def post_view(request, username, post_id):
 
 def post_edit(request, username, post_id):
 
-        # тут тело функции. Не забудьте проверить, 
-        # что текущий пользователь — это автор записи.
-        # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
-        # который вы создали раньше (вы могли назвать шаблон иначе)
         author_profile = User.objects.get(username=username)
-        post = Post.objects.filter(author=author_profile).get(id=post_id)
+        posts_author_profile = Post.objects.filter(author=author_profile)
+        post = get_object_or_404(posts_author_profile,id=post_id)
         if request.user == post.author:
                 form = PostForm({'text': post.text})
                 if request.method == 'POST':
@@ -75,7 +68,7 @@ def post_edit(request, username, post_id):
                         if form.is_valid():
                                 post.text = form.cleaned_data['text']
                                 post.save()
-                                return redirect('post', username, post_id) #redirect('profile', username)
+                                return redirect('post', username, post_id) 
 
                 return render(request, "new.html", {'form':form, 'post': post})
         return redirect('post', username, post_id)
