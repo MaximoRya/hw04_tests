473bd35f7bde1d6b0d1c24ff0db8e3c07462243a
Aleshin <aleshincc@yandex.ru>
2020-03-31 16:53:05 +0300

    Project 4.5: Test coverage

--- posts/views.py
+++ posts/views.py
@@ -8,8 +8,7 @@ from .forms import PostForm
 # Убрал лишнии комментарии, оставил только важные
 
 
-def index(request):
-        
+def index(request):     
         post_list = Post.objects.order_by("-pub_date").all()
         paginator = Paginator(post_list, 10) # показывать по 10 записей на странице.
         page_number = request.GET.get('page') # переменная в URL с номером запрошенной страницы
@@ -18,7 +17,6 @@ def index(request):
         return render(request, 'index.html', {'page': page, 'paginator': paginator})
 
 def group_posts(request, slug):
-
         # функция get_object_or_404 позволяет получить объект из базы данных 
         # по заданным критериям или вернуть сообщение об ошибке если объект не найден
         group = get_object_or_404(Group, slug=slug)
@@ -29,7 +27,6 @@ def group_posts(request, slug):
         return render(request, "group.html", {"group": group, "posts": posts, 'paginator': paginator, 'page': page})
 
 def new_post(request):
-
         if request.user.is_authenticated : #Праверка авторизации
                 if request.method == 'POST':
                         form = PostForm(request.POST)
@@ -41,7 +38,6 @@ def new_post(request):
         return redirect('/') # Если пользователь не авторизован и пытается войти на стр new то его сразу перенаправляет на главную
 
 def profile(request, username):
-
         author_profile = User.objects.get(username=username)
         posts = Post.objects.filter(author=author_profile).order_by("-pub_date")
         paginator = Paginator(posts, 10) 
@@ -50,26 +46,22 @@ def profile(request, username):
         return render(request, "profile.html", {"posts": posts , "username": username, "author_profile": author_profile, 'page': page, 'paginator': paginator})
 
 def post_view(request, username, post_id):
-
         author_profile = User.objects.get(username=username)
         post = Post.objects.filter(author=author_profile).get(id=post_id)
         number = Post.objects.filter(author=author_profile).count()
         return render(request, "post.html", {"post": post , "username": username, "author_profile": author_profile, "number": number})
 
 def post_edit(request, username, post_id):
-
-        author_profile = User.objects.get(username=username)
-        posts_author_profile = Post.objects.filter(author=author_profile)
-        post = get_object_or_404(posts_author_profile,id=post_id)
+        post = get_object_or_404(Post, id=post_id) 
         if request.user == post.author:
-                form = PostForm({'text': post.text})
-                if request.method == 'POST':
-                        form = PostForm(request.POST)
-                        if form.is_valid():
-                                post.text = form.cleaned_data['text']
-                                post.save()
+                form = PostForm(request.POST, instance=post)
+                if request.method == 'POST':                       
+                        if form.is_valid(): 
+                                form.save()
                                 return redirect('post', username, post_id) 
-
-                return render(request, "new.html", {'form':form, 'post': post})
+                form = PostForm(instance=post)               
+                return render(request, "new.html", {'form':form, 'post': post})    
         return redirect('post', username, post_id)
+
+
         
